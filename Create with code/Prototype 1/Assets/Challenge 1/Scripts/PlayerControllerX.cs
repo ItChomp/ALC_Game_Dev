0a57f03d33ae4849808073624c8ca36fcd2c97bc
Curtis <curtis.t317@stu.nebo.edu>
2019-09-23 10:09:04 -0600

    fixed camera following, and made propeller spin

---
+++ Create with code/Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We get the users veritcal input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // we move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow kets
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
--- Create with code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+
+    //Private Variables
+    private float speed = 20f;
+
+    private float turnSpeed = 25.0f;
+
+    private float horizontalInput;
+
+    private float forwardInput;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +24,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We'll move the vechicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        //Where we get the player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+         //We move the vechicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //We turn the vechicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+
+    
     }
 }
