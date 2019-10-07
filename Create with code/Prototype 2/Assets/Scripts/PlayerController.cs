97d18fa7bc2843df4986f733297e980c035dea6b
Curtis <curtis.t317@stu.nebo.edu>
2019-10-07 10:10:53 -0600

    Made Script
    made a script for player controller to move the player.

---
+++ Create with code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float horizontalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -10)
+		{
+			transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+		}
+
+        if (transform.position.x > 10)
+		{
+			transform.position = new Vector3(10, transform.position.y, transform.position.z);
+		}
+
+
+		horizontalInput = horizontalInput.GetAxis("Horizontal");
+
+		transform.Translate(Vector3.right * Time.deltatime * horizontalInput * speed);
+    }
+}
