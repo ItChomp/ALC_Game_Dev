285cb741affd4cdcc453be94612b2671b27cc1e7
Curtis <curtis.t317@stu.nebo.edu>
2019-10-15 10:29:44 -0600

    collision for animals, and game over.

--- Create with code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Create with code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -18,6 +18,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 
         else if (transform.position.z < lowerBounds)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
--- Create with code/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Create with code/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -8,26 +8,32 @@ public class SpawnManager : MonoBehaviour
 	public GameObject[] animalPrefabs;
 	public float spawnRangeX = 20;
 	public float spawnPosZ = 20;
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
 
 
 
 	// Start is called before the first frame update
 	void Start()
 	{
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.S))
-		{
+    // Update is called once per frame
+    void Update()
+    {
 
-			int animalIndex = Random.Range(0, animalPrefabs.Length);
-			Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+    }
 
-			Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
-		}
-	}
+    void SpawnRandomAnimal()
+    {
+
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-10, 10), 0, 20), animalPrefabs[animalIndex].transform.rotation);
+    }
+	
 
 }
